64ff7f2b6d7872c95c44cc126d2d01a40cd6e9ed
YeonjiJung <35820925+YeonjiJung@users.noreply.github.com>
2018-11-29 20:04:01 -0500

    Part 1 - Yeonji's

---
+++ YJ_rf_script.R
@@ -0,0 +1,81 @@
+library(tidyverse)
+library(lubridate)
+library(ROCR)
+library(randomForest)
+library(doParallel)
+library(foreach)
+
+##### Data Cleaning
+  ## load and select the data 
+  rawdf <- read.csv("sqf_08_16.csv")
+  df <- rawdf %>% 
+    filter(suspected.crime=="cpw") %>%
+    select(id, year, found.weapon, 
+           precinct, 
+           location.housing,
+           starts_with("stopped.bc."),
+           starts_with("additional."),
+           suspect.age, suspect.build, suspect.sex, suspect.height, suspect.weight,
+           inside, radio.run, officer.uniform,
+           observation.period,
+           day, month, time.period)
+
+  ## check and convert variable types
+  str(df)
+  df <- df %>% 
+    mutate(id = as.factor(id),
+           found.weapon = as.factor(found.weapon),
+           precinct = as.factor(precinct),
+           time.period = as.factor(time.period))
+  
+  ## change precinct variable into binary one for modelling
+  df_prc <- df %>% 
+    select(id, precinct) %>% mutate(yesno=1) %>% distinct %>%
+    spread(precinct, yesno, fill=0)
+  
+  ## deal with the precinct variable for modeling
+  colnames(df_prc) <- c("id", paste0("precinct", 2:78))
+  
+  sqf <- df %>% left_join(df_prc, by="id") %>% select(-precinct)
+
+##### A
+  ## restrict sqf to years 2013-2014
+  sqf_1314 <- sqf %>% filter(year %in% c(2013,2014))
+  
+  ## randomly shuffle the data
+  set.seed(1314)
+  sqf_1314 <- sqf_1314 %>% slice(sample(1:n()))
+  
+  ## create train_half and test_half
+  splitsize = floor(nrow(sqf_1314)/2)
+  train_half <- sqf_1314 %>% slice(1:splitsize) %>% select(-id,-year)
+  test_half <- sqf_1314 %>% slice(splitsize+1:n()) %>% select(-id,-year)
+  
+  ## restrict sqf to years 2015 and assign it to test_later
+  test_later <- sqf %>% filter(year==2015) %>% select(-id,-year)
+
+##### B
+  ## fit a random forest model on train_half
+  memory.limit(100000)
+  rfmodel <- randomForest(found.weapon ~., data=train_half, ntree=200, na.action=na.omit, proximity=TRUE)
+  
+##### C
+  ## generate predicted probabilities for test_half
+  test_half$predicted.probability <- predict(rfmodel, newdata=test_half, type="response")
+  
+  ## generate predicted probabilities for test_later
+  test_later$predicted.probability <- predict(rfmodel, newdata=test_later, type="response")
+  
+  ## compute the AUC on each test set
+  test_half.pred <- prediction(test_half$predicted.probability, test_half$found.weapon)
+  test_half.perf <- performance(test_half.pred, "auc")
+  auc <- 100*test_half.perf@y.values[[1]]
+  cat('the auc score is ', 100*test_half.perf@y.values[[1]], "\n") 
+  
+  test_later.pred <- prediction(test_later$predicted.probability, test_later$found.weapon)
+  test_later.perf <- performance(test_later.pred, "auc")
+  auc <- 100*test_later.perf@y.values[[1]]
+  cat('the auc score is ', 100*test_half.perf@y.values[[1]], "\n") 
+  
+  
+  
