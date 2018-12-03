dd5c2b4e7de481ebfea81bc59bc1a2ca3d89a265
Hongye Wu <hongyew12@gmail.com>
2018-12-03 17:26:52 -0500

    orgnization

--- rf_script.R
+++ rf_script.R
@@ -1,91 +1,81 @@
-
-### Set Up ###
 library(tidyverse)
 library(lubridate)
-library(randomForest)
 library(ROCR)
+library(randomForest)
+library(doParallel)
+library(foreach)
 
-### Question 1 ###
-raw_df <- read_csv("sqf_08_16.csv")
+##### Data Cleaning
+  ## load and select the data 
+  rawdf <- read.csv("sqf_08_16.csv")
+  df <- rawdf %>% 
+    filter(suspected.crime=="cpw") %>%
+    select(id, year, found.weapon, 
+           precinct, 
+           location.housing,
+           starts_with("stopped.bc."),
+           starts_with("additional."),
+           suspect.age, suspect.build, suspect.sex, suspect.height, suspect.weight,
+           inside, radio.run, officer.uniform,
+           observation.period,
+           day, month, time.period)
 
-### Data Clean Up ###
-sqf <- raw_df %>%
-  filter( suspected.crime == "cpw") %>% 
-  select(id, year, found.weapon, 
-         precinct, 
-         location.housing,
-         starts_with("stopped.bc."),
-         starts_with("additional."),
-         suspect.age, suspect.build, suspect.sex, suspect.height, suspect.weight,
-         inside, radio.run, officer.uniform,
-         observation.period,
-         day, month, time.period)
+  ## check and convert variable types
+  str(df)
+  df <- df %>% 
+    mutate(id = as.factor(id),
+           found.weapon = as.factor(found.weapon),
+           precinct = as.factor(precinct),
+           time.period = as.factor(time.period))
+  
+  ## change precinct variable into binary one for modelling
+  df_prc <- df %>% 
+    select(id, precinct) %>% mutate(yesno=1) %>% distinct %>%
+    spread(precinct, yesno, fill=0)
+  
+  ## deal with the precinct variable for modeling
+  colnames(df_prc) <- c("id", paste0("precinct", 2:78))
+  
+  sqf <- df %>% left_join(df_prc, by="id") %>% select(-precinct)
 
-# Convert variable types as necessary
-sqf <- sqf %>% mutate(id = as.factor(id),
-                      location.housing = as.factor(location.housing),
-                      suspect.build = as.factor(suspect.build),
-                      suspect.sex = as.factor(suspect.sex),
-                      found.weapon = as.factor(found.weapon),
-                      day = as.factor(day),
-                      month = as.factor(month),
-                      time.period = as.factor(time.period),
-                      precinct = as.factor(precinct))
+##### A
+  ## restrict sqf to years 2013-2014
+  sqf_1314 <- sqf %>% filter(year %in% c(2013,2014))
+  
+  ## randomly shuffle the data
+  set.seed(1314)
+  sqf_1314 <- sqf_1314 %>% slice(sample(1:n()))
+  
+  ## create train_half and test_half
+  splitsize = floor(nrow(sqf_1314)/2)
+  train_half <- sqf_1314 %>% slice(1:splitsize) %>% select(-id,-year)
+  test_half <- sqf_1314 %>% slice(splitsize+1:n()) %>% select(-id,-year)
+  
+  ## restrict sqf to years 2015 and assign it to test_later
+  test_later <- sqf %>% filter(year==2015) %>% select(-id,-year)
 
-#convert 'precinct' into binary variables
-precinct_df <- sqf %>% 
-                  select(id, precinct) %>% 
-                  mutate(yesno = 1) %>%
-                  distinct %>% 
-                  spread(precinct, yesno, fill = 0) 
-#change column names for modeling
-colnames(precinct_df) <- c("id", paste0("precinct_", 2:78))             
-
-# join precinct binary variables back to the main dataframe
-sqf_binary <- sqf %>% 
-  left_join(precinct_df, by = "id") %>% 
-  select(-precinct)
-
-### A ###
-sqf_1314 <- sqf_binary %>% filter(year %in% c("2013", "2014"))
-
-#randomly shuffle data
-set.seed(123)
-sqf_set <- sqf_1314 %>% slice(sample(1:n())) 
-
-#split training and test sets
-split_size = floor(nrow(sqf_set)/2)
-train_half <- sqf_set %>% slice(1:split_size) %>% 
-  select(-id, -year)
-test_half <- sqf_set %>% slice(split_size+1:n()) %>% 
-  select(-id, -year)
-
-#restrict to only 2015 data for testing later
-test_later <- sqf_binary %>% filter(year == "2015") %>% 
-  select(-id, -year)
-
-### B ###
-#random forest classification model
-rf_model <- randomForest(found.weapon ~., data = train_half, ntree = 200, na.action = na.omit, proximity = TRUE)
-  # my vector memory exhuasted by trying to run this model. WBU?
-
-### C ###
-
-#predicted probabilities on test data
-test_half$predicted.probability <- predict(rf_model, newdata = test_half, type = "response")
-
-#predicted probabilities on test_later data
-test_later$predicted.probability <- predict(rf_model, newdata = test_later, type = "response")
-
-#AUC
-test_half.pred <- prediction(test_half$predicted.probability, test_half$found.weapon)
-test_half.perf <- performance(test_half.pred, "auc")
-auc <- 100*test_half.perf@y.values[[1]]
-paste0("the auc score for test_half is ", auc)
-
-test_later.pred <- prediction(test_later$predicted.probability, test_later$found.weapon)
-test_later.perf <- performance(test_later.pred, "auc")
-auc <- 100*test_later.perf@y.values[[1]]
-paste0("the auc score for test_later is ", auc)
-
-# NEED EXPLAINATIONS
+##### B
+  ## fit a random forest model on train_half
+  memory.limit(100000)
+  rfmodel <- randomForest(found.weapon ~., data=train_half, ntree=200, na.action=na.omit, proximity=TRUE)
+  
+##### C
+  ## generate predicted probabilities for test_half
+  test_half$predicted.probability <- predict(rfmodel, newdata=test_half, type="response")
+  
+  ## generate predicted probabilities for test_later
+  test_later$predicted.probability <- predict(rfmodel, newdata=test_later, type="response")
+  
+  ## compute the AUC on each test set
+  test_half.pred <- prediction(test_half$predicted.probability, test_half$found.weapon)
+  test_half.perf <- performance(test_half.pred, "auc")
+  auc <- 100*test_half.perf@y.values[[1]]
+  cat('the auc score is ', 100*test_half.perf@y.values[[1]], "\n") 
+  
+  test_later.pred <- prediction(test_later$predicted.probability, test_later$found.weapon)
+  test_later.perf <- performance(test_later.pred, "auc")
+  auc <- 100*test_later.perf@y.values[[1]]
+  cat('the auc score is ', 100*test_half.perf@y.values[[1]], "\n") 
+  
+  
+  
